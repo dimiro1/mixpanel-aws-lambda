5d086a577da7f73a89b5505f4eef7df0fad41d4e
Claudemiro <dimiro1@gmail.com>
2016-06-19 15:24:40 -0300

    Added tracker lambda function

---
+++ index.js
@@ -0,0 +1,86 @@
+// https://mixpanel.com/help/reference/http
+
+const AWS = require('aws-sdk');
+
+const Firehose = new AWS.Firehose();
+
+const SUCCESS = 1;
+const ERROR   = 0;
+
+const config = {
+    deliveryStreamName: 'Mixpanel' 
+};
+
+// {
+//     "data": "eyJldmVudCI6ICJnYW1lIiwgInByb3BlcnRpZXMiOiB7ImlwIjogIjEyMy4xMjMuMTIzLjEyMyIsICJkaXN0aW5jdF9pZCI6IDEzNzkzLCAidG9rZW4iOiAiZTNiYjQxMDAzMzBjMzU3MjI3NDBmYjhjNmY1YWJkZGMiLCAidGltZSI6IDEyNDU2MTM4ODUsICJhY3Rpb24iOiAicGxheSJ9fQ==",
+//     "ip": "127.0.0.1",
+//     "redirect": "",
+//     "callback": "",
+//     "img": false,
+//     "verbose": false
+// }
+//
+// Example result
+// Success Verbose
+// { "status": 1, "error": null }
+// Success not verbose
+// 1
+// Error verbose
+// {"status": 0, "error": "time, 2554.7 days in the past"}
+// Error not verbose
+// 0
+exports.handler = function handler(event, context, callback) {
+    const ip = event.ip || "127.0.0.1";
+    
+    // Not implemented
+    const redirect = event.redirect || "";
+    const verbose = event.verbose || false;
+    
+    console.log({ip, redirect, verbose});
+    
+    if (event.callback) {
+        callback(null, output(verbose, ERROR, "callback, jsonp not supported in this implementation"));
+        return;
+    }
+    
+    if (event.img) {
+        callback(null, output(verbose, ERROR, "img, img is not supported in this implementation"));
+        return;
+    }
+    
+    if (!event.data) {
+        callback(null, output(verbose, ERROR, "data, missing or empty"));
+        return;
+    }
+    
+    try {
+        const data = JSON.parse(new Buffer(event.data, "base64").toString("utf8"));
+    } catch (e) {
+        callback(null, output(verbose, ERROR, "data, invalid base64 encoding"));
+        return;
+    }
+    
+    var params = {
+        DeliveryStreamName: config.deliveryStreamName,
+        Record: {
+            Data: JSON.stringify(data)
+        }
+    };
+    
+    Firehose.putRecord(params, (err, _data) => {
+        if (err) {
+            console.error(err);
+            callback(null, output(verbose, ERROR, "firehose, could not send data to firehose"));
+        } else {
+            callback(null, output(verbose, SUCCESS, null));
+        }
+    });
+};
+
+function output(verbose, status, error) {
+    if (verbose) {
+        return { status, error };
+    }
+    
+    return status;
+}
