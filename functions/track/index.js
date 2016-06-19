0766da790f1ed64712ecb7d532982e5877221402
claudemiro <dimiro1@gmail.com>
2016-06-19 20:37:33 -0300

    Added linter

--- functions/track/index.js
+++ functions/track/index.js
@@ -5,7 +5,15 @@ const AWS = require('aws-sdk');
 const Firehose = new AWS.Firehose();
 
 const SUCCESS = 1;
-const ERROR   = 0;
+const ERROR = 0;
+
+function output(verbose, status, error) {
+    if (verbose) {
+        return { status, error };
+    }
+
+    return status;
+}
 
 // {
 //     "data": "eyJldmVudCI6ICJnYW1lIiwgInByb3BlcnRpZXMiOiB7ImlwIjogIjEyMy4xMjMuMTIzLjEyMyIsICJkaXN0aW5jdF9pZCI6IDEzNzkzLCAidG9rZW4iOiAiZTNiYjQxMDAzMzBjMzU3MjI3NDBmYjhjNmY1YWJkZGMiLCAidGltZSI6IDEyNDU2MTM4ODUsICJhY3Rpb24iOiAicGxheSJ9fQ==",
@@ -16,57 +24,65 @@ const ERROR   = 0;
 //     "verbose": false
 // }
 exports.handle = function handle(event, context, callback) {
-    const ip = event.ip || "127.0.0.1";
-    
+    const ip = event.ip || '127.0.0.1';
+
     // Not implemented
-    const redirect = event.redirect || "";
+    const redirect = event.redirect || '';
     const verbose = event.verbose || false;
-    
-    console.log({ip, redirect, verbose});
-    
+    var data = '';
+
+    console.log({ ip, redirect, verbose });
+
     if (event.callback) {
-        callback(null, output(verbose, ERROR, "callback, jsonp not supported in this implementation"));
+        callback(null,
+            output(verbose,
+                ERROR,
+                'callback, jsonp not supported in this implementation'
+            )
+        );
         return;
     }
-    
+
     if (event.img) {
-        callback(null, output(verbose, ERROR, "img, img is not supported in this implementation"));
+        callback(null,
+            output(verbose,
+                ERROR,
+                'img, img is not supported in this implementation'
+            )
+        );
         return;
     }
-    
+
     if (!event.data) {
-        callback(null, output(verbose, ERROR, "data, missing or empty"));
+        callback(null, output(verbose, ERROR, 'data, missing or empty'));
         return;
     }
-    
+
     try {
-        const data = JSON.parse(new Buffer(event.data, "base64").toString("utf8"));
+        data = JSON.parse(new Buffer(event.data, 'base64').toString('utf8'));
     } catch (e) {
-        callback(null, output(verbose, ERROR, "data, invalid base64 encoding"));
+        callback(null, output(verbose, ERROR, 'data, invalid base64 encoding'));
         return;
     }
-    
-    var params = {
+
+    const params = {
         DeliveryStreamName: process.env.DELIVERY_STREAM_NAME,
         Record: {
-            Data: JSON.stringify(data)
-        }
+            Data: JSON.stringify(data),
+        },
     };
-    
-    Firehose.putRecord(params, (err, _data) => {
+
+    Firehose.putRecord(params, (err) => {
         if (err) {
             console.error(err);
-            callback(null, output(verbose, ERROR, "firehose, could not send data to firehose"));
+            callback(null,
+                output(verbose,
+                    ERROR,
+                    'firehose, could not send data to firehose'
+                )
+            );
         } else {
             callback(null, output(verbose, SUCCESS, null));
         }
     });
 };
-
-function output(verbose, status, error) {
-    if (verbose) {
-        return { status, error };
-    }
-    
-    return status;
-}
